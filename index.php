12a86ae290d17552ccb680fe05a5c508151f083d
Manish <manish@Mac.attlocal.net>
2025-06-03 19:05:19 -0500

    Code Push for updte

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
 
-    <title>MAX LAMBDA Company Limited - GHANA
+    <title>Packerslc
 </title>
 
     <!--Favicon-->
